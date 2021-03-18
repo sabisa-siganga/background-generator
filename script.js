fb0961ea3eb468db18e64c3ed52dbfa0a664b588
Sabisa <lumisabisa@gmail.com>
2021-03-18 10:13:26 +0200

    adding stating project

---
+++ script.js
@@ -0,0 +1,30 @@
+var database = [
+    {
+    	username: "andrei",
+    	password: "supersecret"
+    }
+];
+
+var newsFeed = [
+    {
+    	username: "Bobby",
+    	timeline: "So tired from all that learning"
+    },
+    {
+    	username: "Sally",
+    	timeline: "Javacript is so cooool"
+    }
+];
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password");
+
+function signIn(user,pass){
+    if (user === database[0].username&& pass === database[0].password){
+        console.log(newsFeed);       
+    } else{
+        alert("Sorry, wrong username and password!");
+    }
+}
+
+signIn(userNamePrompt, passwordPrompt); 
